f3291df859c12a955d4ba0fa56dbec4f9a9fdd0e
Arjun Mohanan <arj333un@gmail.com>
2026-01-09 16:16:06 +0530

    yeild commit

---
+++ resources/views/admin/club.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.components.app')
+
+@section('content')
+    <h1>Clubs</h1>
+
+    <label>Name</label>
+    <input type="text">
+
+    <label>State</label>
+    <input type="text">
+    
+    <input type="submit" name="submit">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\ClubMember;
+use App\Http\Controllers\SampleController;
+
 Route::get('/', function () {
-    return view('welcome');
+     return view('admin.club');
 });
+
+// Route::get('/insert', function () {
+//     return view("clubm");
+// })->name('insert');
+
+//Route::POST('/store',[SampleController::class,'store'])->name('asda');
+// Route::POST('/app',[SampleController::class,'store'])->name('app');
+
+
+
