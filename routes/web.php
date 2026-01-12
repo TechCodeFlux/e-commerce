94e8dd424270919a82930083d4fae9d11e5237ad
Pauljo George <129671514+TechCodeFlux@users.noreply.github.com>
2026-01-12 11:45:04 +0530

    eight commit(login ui)

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,18 @@ use App\Http\Controllers\DevelopmentController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClubController;
 
-Route::get('/', function () {
-    return view('adminlogin');
-});
+Route::get('/', function () {return view('auth.login');});
+// Route::get('/', function () {return view('adminlogin');});
 
+Route::prefix('admin')->name('admin.')->namespace('App\Http\Controllers\Admin')->group(function () {
+    Auth::routes(['register' => false]);});
 
 Route::resource('admin',DevelopmentController::class);
-
 Route::post('admin', [AdminController::class, 'login']);
-
 Route::resource('club', ClubController::class);
 Route::get('/clubs', [ClubController::class, 'index'])->name('club.index');
 Route::post('/club', [ClubController::class, 'store'])->name('club.store');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
