b3391fc7161d937817b83b2e90ffeddda45f9769
Pauljo George <129671514+TechCodeFlux@users.noreply.github.com>
2026-01-23 11:31:21 +0530

    clubmember

---
+++ app/Http/Controllers/Club/ClubMemberController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Club;
+
+use App\Http\Controllers\Controller;
+use App\Models\ClubMember;
+use Illuminate\Http\Request;
+
+class ClubMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('club.clubmember.clubmemberview');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $clubmember = new ClubMember();
+        return view('club.clubmember.form', compact('clubmember'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClubMember $clubMember)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClubMember $clubMember)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ClubMember $clubMember)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClubMember $clubMember)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Admin\DashboardController;
+
 use App\Http\Controllers\Admin\ClubController;
+//for dashboard
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Club\ClubDashboardController;
+//for login
 use App\Http\Controllers\Club\Auth\LoginController as ClubLoginController;
 use App\Http\Controllers\Admin\Auth\LoginController as AdminLoginController;
+//for add user
+use App\Http\Controllers\Club\ClubMemberController;
+
 // use App\Http\Controllers\Admin\Auth\LoginController as ClubMemberLoginController;
 
 //arjun
@@ -19,29 +25,13 @@ Route::prefix('club')->name('club.')->namespace('App\Http\Controllers\Club')->gr
     Auth::routes(['register' => false]); 
     //dashboard controller
     Route::get('dashboard', [ClubDashboardController::class, 'index'])->name('dashboard');//dashboard
+    //club controller
+    Route::get('clubmembers', [ClubMemberController::class, 'index'])->name('clubmembersindex'); //view club members in table
+    Route::get('clubmembersform', [ClubMemberController::class, 'create'])->name('clubmember'); //To add club member data form(submit form) 
 
 });
 //pauljo
-// Admin login
-Route::get('/admin', function () {return view('admin.auth.login');})->name('admin.login');
-Route::post('/admin', [AdminLoginController::class, 'login'])->name('admin.login.submit');
-Route::post('/admin/logout', [AdminLoginController::class, 'logout'])->name('admin.logout');
-
-Route::prefix('admin')->name('admin.')->namespace('App\Http\Controllers\Admin')->group(function () {
-    Auth::routes(['register' => false]);    
-    //dashboard controller
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');//dashboard
-    Route::get('profile', [DashboardController::class, 'profile'])->name('profile');//profile view
-    Route::post('profile', [DashboardController::class, 'profile_update'])->name('profile_update');//profile update
-    //admin club controller
-    Route::get('clubs', [ClubController::class, 'index'])->name('clubsindex'); //view clubs in table 
-    Route::get('clubsform', [ClubController::class, 'create'])->name('club'); //To add club data form(submit form)
-    Route::get('clubsform/{club}', [ClubController::class, 'edit'])->name('editclub'); //To add club data form for edit(update form) 
-    Route::post('clubsadd', [ClubController::class, 'store'])->name('addclub'); //add club data to table (submit form)
-    Route::put('clubsupdate/{club}', [ClubController::class, 'update'])->name('update'); //add club data (update form)
-    Route::get('/get-states/{country}', [ClubController::class, 'getStates'])->name('get.states');//get states based on country ID
-});
-
+                                                                                                                                                                                                                                                                                                               
 
 
 //aishwarya
