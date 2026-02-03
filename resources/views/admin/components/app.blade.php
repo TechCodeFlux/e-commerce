68cdd03038aa7679c02faed465366a4bc976263e
adarsh-pathrose <adarshpathrose03@gmail.com>
2026-02-03 17:37:06 +0530

    login completed admin,club

--- resources/views/admin/components/app.blade.php
+++ resources/views/admin/components/app.blade.php
@@ -66,7 +66,7 @@
 <!-- Main Javascript file -->
 <script src="{{url('dist/js/app.min.js')}}"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
 
 @yield('script')
 </body>
