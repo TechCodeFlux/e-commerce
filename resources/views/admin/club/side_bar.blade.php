4b100ce11c4236c90a3f96223e50dab2726cf5d6
adarsh-pathrose <adarshpathrose03@gmail.com>
2026-02-10 17:01:05 +0530

    club dashboard in admin

--- resources/views/admin/club/side_bar.blade.php
+++ resources/views/admin/club/side_bar.blade.php
@@ -3,7 +3,7 @@
         <div class="card-body">
             <ul class="nav nav-pills flex-column gap-2" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="profile-tab" href="" role="tab" aria-controls="profile" aria-selected="true">
+                    <a class="nav-link" id="profile-tab" href="{{ route('admin.clubs.dashboard', $club->id) }}" role="tab" aria-controls="profile" aria-selected="true">
                         <i class="bi-graph-up me-2"></i> Dashboard
                     </a>
                 </li>
